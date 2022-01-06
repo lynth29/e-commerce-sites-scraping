f20fc4c751ff8ceaa2068e131682ee0aa8dd7f24
Ly Nguyen <lynth.work@gmail.com>
2022-01-06 09:41:22 +0700

    Config chromedriver again

--- helpers/crawl.py
+++ helpers/crawl.py
@@ -33,7 +33,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import SessionNotCreatedException, TimeoutException, NoSuchElementException, StaleElementReferenceException
-IGNORED_EXCEPTIONS = (NoSuchElementException, StaleElementReferenceException)
+IGNORED_EXCEPTIONS = (NoSuchElementException, StaleElementReferenceException, TimeoutException)
 
 # Parameters
 PROJECT_PATH = Path(__file__).absolute().parents[1]
@@ -54,11 +54,10 @@ class ChromeDriver:
         op.add_argument("--window-size=1920,1080")
         # 6 arguments to optimize speed when loading
         op.add_argument('--no-sandbox')
-        op.add_argument("start-maximized")
+        op.add_argument("--disable-infobars")
         op.add_argument("--disable-extensions")
         op.add_argument("--disable-gpu")
         op.add_argument("--disable-dev-shm-usage")
-        op.add_argument("--enable-javascript")
 
         # Create a driver based on chromedriver exe file and options
         if platform.machine() == "arm64":
