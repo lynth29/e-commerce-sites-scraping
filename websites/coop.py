04c906c4052cde652af0f1570dedf482722c3a63
Nguyễn Thị Hương Ly <ly.nth2@citigo.com.vn>
2022-10-28 18:42:29 +0700

    refactor: change find_element_by_xpath to find_element(By.XPAH

--- websites/coop.py
+++ websites/coop.py
@@ -16,8 +16,6 @@ from helpers.crawl import *
 # Parameters
 SITE_NAME = "coop"
 PATH_CSV = os.path.join(PROJECT_PATH, "csv", SITE_NAME)
-CLEAN_CSV = os.path.join(PROJECT_PATH, "clean_csv", SITE_NAME)
-
 
 # Define class
 class Coop:
@@ -41,19 +39,25 @@ class Coop:
         self.BROWSER.get(self.BASE_URL)
         sleep(2)
         # Choose mart
-        mart = Select(self.BROWSER.find_element_by_xpath("(//select)[2]"))
+        district = Select(self.BROWSER.find_element(By.XPATH, "(//select)[2]"))
+        district.select_by_index(1)  # 01
+        sleep(2)
+        ward = Select(self.BROWSER.find_element(By.XPATH, "(//select)[3]"))
+        ward.select_by_index(1)  # Ben Thanh
+        sleep(2)
+        mart = Select(self.BROWSER.find_element(By.XPATH, "(//select)[4]"))
         mart.select_by_index(1)  # Tan Phong
         sleep(2)
         # Click button
-        self.BROWSER.find_element_by_xpath("//button[contains(text(), 'Chọn')]").click()
+        self.BROWSER.find_element(By.XPATH, "//button[contains(text(), 'Xác nhận')]").click()
         sleep(2)
 
     def disable_sub(self):
         """Disable subscription popup"""
         try:
             self.wait.until(
-                EC.presence_of_element_located((By.ID, "onesignal-slidedown-dialog")))
-            self.BROWSER.find_element(By.XPATH,     "//button[contains(@class, 'align-right secondary')]").click()
+                EC.presence_of_element_located((By.ID, "slidedown-footer")))
+            self.BROWSER.find_element(By.XPATH, "//button[contains(@id, 'onesignal-slidedown-cancel-button')]").click()
         except TimeoutException:
             pass
 
@@ -63,30 +67,24 @@ class Coop:
         self.BROWSER.get(self.BASE_URL)
         sleep(5)
         # Get soup
-        toppage_soup = BeautifulSoup(self.BROWSER.page_source, "lxml")
+        toppage_soup = BeautifulSoup(self.BROWSER.page_source, features="lxml")
         # Get categories
-        categories_bar = [toppage_soup.findAll("ul", {'class': 'megamenu'})]
-        categories_bar = [
-            item for sublist in categories_bar for item in sublist]
-        categories_bar = [categories_bar[1]]
-        categories_tag = [cat.findAll('li') for cat in categories_bar]
-        categories_tag = [
-            item for sublist in categories_tag for item in sublist]
-        categories = [cat.find_all('a') for cat in categories_tag]
-        categories = [item for sublist in categories for item in sublist]
-        categories = categories[2:]
+        categories_bar = toppage_soup.find("ul", {'class': 'megamenu'}).find_all('li')
         # Create an empty list to store categories' information
         page_list = []
-        for cat in categories:
-            next_page = {}
-            link = re.sub(".+cooponline\\.vn", "", cat['href'])
-            next_page['relativelink'] = link
-            next_page['directlink'] = self.BASE_URL + link
-            name = re.sub("/|\\?.=", "_", link)
-            next_page['name'] = re.sub("_production", "", name)
-            next_page['label'] = re.sub(
-                "\n                                                                                                            |\n", "", cat.text)
-            page_list.append(next_page)
+        for cat in categories_bar:
+            cat_l1 = cat.find('a').text.strip()
+            cat_l2s = cat.find_all('div', class_="menu")
+            for child_cat in cat_l2s:
+                cat_l2 = child_cat.find('a', class_="main-menu").text.strip()
+                cat_l3s = child_cat.find_all('a', class_=None)
+                for grandchild_cat in cat_l3s:
+                    row = {}
+                    row['cat_l1'] = cat_l1
+                    row['cat_l2'] = cat_l2
+                    row['cat_l3'] = grandchild_cat.text.strip()
+                    row['href'] = grandchild_cat['href']
+                    page_list.append(row)
         # Remove duplicates
         page_list = [dict(t) for t in set(tuple(i.items()) for i in page_list)]
         return page_list
@@ -94,133 +92,57 @@ class Coop:
     def scrap_data(self, cat):
         """Get item data from a category page and self.write to csv"""
         # Access
-        self.BROWSER.get(cat['directlink'])
+        self.BROWSER.get(cat['href'])
         # Get soup
         soup = BeautifulSoup(self.BROWSER.page_source, 'lxml')
-        # Get category name
-        cat_name = soup.find('h3', {'class': 'title-category'})
-        attempts_limit = 5
-        if cat_name == None:
-            attempts = 0
-            while attempts < attempts_limit:
-                try:
-                    cat_name = cat_name.text
-                except:
-                    attempts += 1
-                    print("Try getting cat_name again")
-            else:
-                print(f"Cannot download get cat_name")
-                cat_name = ""
-        else:
-            cat_name = cat_name.text
         # Click see_more button as many as possible
         while True:
             try:
                 # Wait
                 self.wait.until(
-                    EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'btn-success')]")))
-                see_more = self.BROWSER.find_element(By.XPATH, "//button[contains(@class, 'btn-success')]")
+                    EC.presence_of_element_located((By.XPATH, "//span[contains(text(), ' Xem tiếp . . .')]")))
+                see_more = self.BROWSER.find_element(By.XPATH, "/span[contains(text(), ' Xem tiếp . . .')]")
                 see_more.click()
             except IGNORED_EXCEPTIONS:
                 print(
-                    'Clicked all see_more button as much as possible in ' + cat_name + ' category.')
+                    'Clicked all see_more button as much as possible in ' + cat['cat_l3'] + ' category.')
                 break
         # Scraping product's data
-        try:
-            soup = BeautifulSoup(self.BROWSER.page_source, 'lxml')
-            sleep(10)
-            # Get all products' holders
-            list = soup.find_all('div', {'class': 'product-item-container'})
-            print('Found ' + str(len(list)) + ' products')
-            # Get main category and sub-category
-            directory = soup.find_all('span', {'property': 'name'})
-            if len(directory) != 1:
-                subdir = directory[1].text.strip()
-            else:
-                subdir = directory[0].text.strip()
-            # Scraping data
-            for item in list:
-                row = {}
-                # Name
-                if item.find('h4', {'class': 'title_product_lmh'}) != None:
-                    good_name = item.find(
-                        'h4', {'class': 'title_product_lmh'}).text
-                    row['good_name'] = good_name
-                else:
-                    None
-                # Price
-                if item.find('span', class_='price-new') != None:
-                    price = item.find('span', class_='price-new').text.strip()
-                    price = price.split('đ')[0]
-                    price = price.strip()
-                    row['price'] = price
-                else:
-                    None
-                # Old price
-                if item.find('span', class_='price-old') != None:
-                    old_price = item.find(
-                        'span', class_='price-old').text.strip()
-                    old_price = old_price.split('đ')[0]
-                    old_price = old_price.strip()
-                    row['old_price'] = old_price
-                else:
-                    None
-                # ID
-                if item.find('a', {'target': '_self'}) != None:
-                    item_id = item.find('a', {'target': '_self'}).get('href')
-                    item_id = re.sub(".+products/|/", "", item_id)
-                    row['id'] = item_id
-                # Sub category
-                row['parent_category'] = subdir
-                # Category
-                row['category'] = cat['label']
-                # Date
-                row['date'] = self.DATE
-                self.OBSERVATION += 1
-                self.wr.write_data(row)
-            print('Finished scraping ' + cat_name + ' category.')
-        except Exception as e:
-            print("Error on " + self.BROWSER.current_url)
-            print(type(e).__name__ + str(e))
-            pass
-
-    def cleaning_data(self):
-        # Import CSV file
-        os.chdir(PATH_CSV)
-        raw = pd.read_csv(SITE_NAME + '_' + self.DATE + '.csv')
-        print('Imported CSV file to a dataframe')
-
-        # Summarize the dataframe
-        print(
-            f"The dataframe has {raw.shape[0]} rows and {raw.shape[1]} columns.")
-        print('Have a look at the dtypes:')
-        print(raw.info())
-        print('Are there any null value?:')
-        print(raw.isnull().any())
-
-        # Convert dtypes
-        # Date
-        raw.date = str(datetime.date.today())
-        raw.date = pd.to_datetime(raw.date)
-        # Price
-        if raw.price.str.contains('Đang cập nhật').any():
-            raw.price = np.where(raw.price == 'Đang cập nhật', 0, raw.price)
-        if raw.price.str.contains('.').any():
-            raw.price = raw.price.str.replace('.', '', regex=True)
-        if raw.price.str.contains(',').any():
-            raw.price = raw.price.str.replace(',', '', regex=True)
-        raw.price = raw.price.astype(float)
-        # Old price
-        if raw.old_price.isnull().any():
-            raw.old_price = raw.old_price.fillna(0)
-            raw.old_price = np.where(
-                raw.old_price == 0, raw.price, raw.old_price)
-        print('Have a look at the dtypes after converting:')
-        print(raw.info())
-        os.remove(SITE_NAME + '_' + self.DATE + '.csv')
-
-        # Export to new CSV
-        os.chdir(PROJECT_PATH)
-        os.chdir(CLEAN_CSV)
-        raw.to_csv(SITE_NAME + '_' + self.DATE + '_clean.csv')
-        print('Finished cleaning data')
+        # try:
+        soup = BeautifulSoup(self.BROWSER.page_source, features='lxml')
+        sleep(10)
+        # Get all products' holders
+        list = soup.find_all('div', {'class': 'product-item-container'})
+        print('Found ' + str(len(list)) + ' products')
+        # Scraping data
+        for item in list:
+            row = {}
+            row['cat_l1'] = cat['cat_l1']
+            row['cat_l2'] = cat['cat_l2']
+            row['cat_l3'] = cat['cat_l3']
+            # Name
+            product_name = item.find('h4').text.strip() if item.find('h4') != None else None
+            # # Price
+            # if item.find('span', class_='price-new col-xs-12') != None:
+            #     price = item.find('span', class_='price-new col-xs-12').text.strip()
+            #     price = price.split('đ')[0].strip()
+            #     price = price.replace(',',"")
+            #     row['price'] = int(price)
+            # else:
+            #     None
+            # # Old price
+            # if item.find('span', class_='price-old') != None:
+            #     old_price = item.find(
+            #         'span', class_='price-old').text.strip()
+            #     old_price = old_price.split('đ')[0].strip()
+            #     old_price = old_price.replace(',',"")
+            #     row['old_price'] = int(old_price)
+            # else:
+            #     None
+            self.OBSERVATION += 1
+            self.wr.write_data(row)
+        print('Finished scraping ' + cat['cat_l3'] + ' category.')
+        # except Exception as e:
+        #     print("Error on " + self.BROWSER.current_url)
+        #     print(type(e).__name__ + str(e))
+        #     pass
